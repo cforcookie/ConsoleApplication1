74d544d0ab3f095399614346a24672cba07d47c1
flotry <jastbast1103@outlook.com>
2022-03-17 14:04:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+int summ(int number)
+{
+    int start = 0;
+    for (int i = 0; i < number; i++)
+    {
+        start = start + i;
+    }
+    return(start);
+}
+
+int main()
+{
+    int number;
+    std::cout << "Write number to summ: ";
+    std::cin >> number;
+    int result = summ(number);
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
