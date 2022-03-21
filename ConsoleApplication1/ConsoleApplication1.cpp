914059a48ee7b4f68fc823584d3ca037f81e87eb
flotry <jastbast1103@outlook.com>
2022-03-21 14:11:18 +0300

    Код закоментирован

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <string>
-
+//Моудль суммы чисел до конкретного числа, принемает число до которого ведется счет
 int summ(int number)
 {
     int start = 0;
@@ -13,10 +13,11 @@ int summ(int number)
 
 int main()
 {
-    int number;
+    int number; //Число до которого будет идти счет
+    //Ввод числа 
     std::cout << "Write number to summ: ";
     std::cin >> number;
-    int result = summ(number);
-    std::cout << "Result: " << result << std::endl;
+    int result = summ(number); //Присваивает результат вычисления функции 
+    std::cout << "Result: " << result << std::endl; //Вывод результата на экран
     return 0;
 }
